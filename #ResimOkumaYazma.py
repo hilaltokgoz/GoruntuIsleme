e02440ec38f4b32e926f12b613e1be2229345704
Hilal <hilaltokgoz98@gmail.com>
2025-01-16 13:58:37 +0300

    resimOkuma

---
+++ #ResimOkumaYazma.py
@@ -0,0 +1,23 @@
+#Resim Okuma Yazma
+
+import cv2 #opencv 
+
+#Görseli Alır
+img = cv2.imread('mi.jpeg')
+cv2.imshow('Profil', img)
+
+#Görseli Direkt Gri Alır.
+#img = cv2.imread('mi.jpeg',cv2.IMREAD_GRAYSCALE) # =img = cv2.imread('mi.jpeg',0)
+#cv2.imshow('Profil', img)
+
+#resmi kapatma
+#cv2.waitKey(0) #event gelene kadar resim durur
+#cv2.destroyAllWindows()
+
+#koşula bağlı  kapanma istersek
+kInp = cv2.waitKey(0) 
+if kInp == ord('a'): 
+    print('a tusunua basıldı')
+else:
+    print('Baska tusa basıldı')
+cv2.destroyAllWindows()
