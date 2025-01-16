913cb7ae531e2abddb35d28e4e818e74485d92b4
Hilal <hilaltokgoz98@gmail.com>
2025-01-16 15:30:27 +0300

    newCommit

---
+++ #KameradanGörüntüEldeEtme.py
@@ -0,0 +1,27 @@
+#Kameradan görüntü elde etme
+
+import cv2
+
+vid = cv2.VideoCapture(0) #kameradan görüntü al
+
+w= int(vid.get(3)) 
+h = int(vid.get(4))
+size = (w,h)
+
+result = cv2.VideoWriter('record.avi',cv2.VideoWriter_fourcc(*'XVID'),24,size)
+
+while True:
+    ret,frame = vid.read()
+
+    if ret == True:
+        result.write(frame)
+        cv2.imshow('frame',frame)
+        kInp = cv2.waitKey(1)
+        if kInp == ord('s'):
+            break
+    else:
+        break
+
+vid.release()
+result.release()
+cv2.destroyAllWindows()
