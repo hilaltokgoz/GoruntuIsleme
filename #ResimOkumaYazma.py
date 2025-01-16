d84c8b8bdb3beb908ad51ebdbbf6747180a71d87
Hilal <hilaltokgoz98@gmail.com>
2025-01-16 14:03:19 +0300

    resimKaydetme

--- #ResimOkumaYazma.py
+++ #ResimOkumaYazma.py
@@ -11,13 +11,18 @@ cv2.imshow('Profil', img)
 #cv2.imshow('Profil', img)
 
 #resmi kapatma
-#cv2.waitKey(0) #event gelene kadar resim durur
-#cv2.destroyAllWindows()
+cv2.waitKey(0) #event gelene kadar resim durur
+cv2.destroyAllWindows()
 
 #koşula bağlı  kapanma istersek
-kInp = cv2.waitKey(0) 
-if kInp == ord('a'): 
-    print('a tusunua basıldı')
-else:
-    print('Baska tusa basıldı')
-cv2.destroyAllWindows()
+# kInp = cv2.waitKey(0) 
+# if kInp == ord('a'): 
+#     print('a tusunua basıldı')
+# else:
+#     print('Baska tusa basıldı')
+# cv2.destroyAllWindows()
+
+#resmi kaydetme
+img = cv2.imread('mi.jpeg',cv2.IMREAD_GRAYSCALE)
+cv2.imwrite('mi2.jpeg',img) #mi2 de kaydedildi
+
